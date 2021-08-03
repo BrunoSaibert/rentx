321af182d4260fd24276f3621a7430eb3d24fb86
Bruno Saibert <brunosaibert@gmail.com>
2021-08-02 23:55:43 -0300

    feat: add stack navigator

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import "react-native-gesture-handler";
+
 import React from "react";
 import AppLoading from "expo-app-loading";
 import { ThemeProvider } from "styled-components";
@@ -15,11 +17,7 @@ import {
 
 import theme from "./src/styles/theme";
 
-import { Home } from "./src/screens/Home";
-import { CarDetails } from "./src/screens/CarDetails";
-import { Scheduling } from "./src/screens/Scheduling";
-import { SchedulingDetails } from "./src/screens/SchedulingDetails";
-import { SchedulingComplete } from "./src/screens/SchedulingComplete";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -36,7 +34,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingComplete />
+      <Routes />
     </ThemeProvider>
   );
 }
