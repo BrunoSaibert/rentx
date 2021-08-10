2eabb0335cd8f518d99e0f346c52404e4f68cf11
Bruno Saibert <brunosaibert@gmail.com>
2021-08-09 22:43:53 -0300

    feat: animation car details scroll

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { StatusBar } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  interpolate,
+  Extrapolate,
+} from "react-native-reanimated";
 
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
@@ -22,6 +29,28 @@ export function CarDetails() {
   const route = useRoute();
   const { car } = route.params as Params;
 
+  const scrollY = useSharedValue(0);
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    scrollY.value = event.contentOffset.y;
+  });
+
+  const headerStyleAnimation = useAnimatedStyle(() => {
+    return {
+      height: interpolate(
+        scrollY.value,
+        [0, 200],
+        [180, 86],
+        Extrapolate.CLAMP
+      ),
+    };
+  });
+
+  const sliderCasStyleAnimation = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(scrollY.value, [20, 100], [1, 0], Extrapolate.CLAMP),
+    };
+  });
+
   function handleConfirmRental() {
     navigation.navigate("Scheduling", { car });
   }
@@ -33,15 +62,27 @@ export function CarDetails() {
         backgroundColor="transparent"
         translucent
       />
-      <S.Header>
-        <BackButton />
-      </S.Header>
 
-      <S.CarImages>
-        <ImageSlider imagesUrl={car.photos} />
-      </S.CarImages>
+      <Animated.View style={[headerStyleAnimation]}>
+        <S.Header>
+          <BackButton />
+        </S.Header>
 
-      <S.Content>
+        <Animated.View style={[sliderCasStyleAnimation]}>
+          <S.CarImages>
+            <ImageSlider imagesUrl={car.photos} />
+          </S.CarImages>
+        </Animated.View>
+      </Animated.View>
+
+      <Animated.ScrollView
+        contentContainerStyle={{
+          padding: 24,
+        }}
+        showsVerticalScrollIndicator={false}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
+      >
         <S.Details>
           <S.Description>
             <S.Brand>{car.brand}</S.Brand>
@@ -65,7 +106,8 @@ export function CarDetails() {
         </S.Accessories>
 
         <S.About>{car.about}</S.About>
-      </S.Content>
+        <S.About>{car.about}</S.About>
+      </Animated.ScrollView>
 
       <S.Footer>
         <Button
